c9de7bdfe16137d193354f611c9660126f5e0e18
dohun <ehgns1524@naver.com>
2024-12-26 19:36:58 +0900

    study array, oop
    study array, oop

---
+++ Array.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int sumsum(int arr[], int size)
+{
+	int sum = 0;
+	for (int i = 0; i < size; i++)
+	{
+		sum = sum + arr[i];
+	}
+	return sum;
+}
+
+int avgavg(int sum, int size)
+{
+	int avg = 0;
+
+	avg = sum / size;
+
+	return avg;
+}
+
+int main()
+{
+	int arr[5];
+	int size, sum, avg = 0;
+	
+	cout << "입력을 원하는 숫자의 개수 : ";
+	cin >> size;
+	cout << "숫자" << size <<"개를 입력하시오." << endl;
+	for (int i = 0; i < size; i++)
+	{
+		cin >> arr[i];
+	}
+
+	sum = sumsum(arr, size);
+	avg = avgavg(sum, size);
+	cout << "합 : " << sum << endl;
+	cout << "평균 : " << avg << endl;
+	return 0;
+}
---
+++ OOP.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+using namespace std;
+
+
+class Animal {
+public:
+	Animal() { cout << "Animal 기본 생성자 호출!" << endl; }
+	virtual void makeSound() = 0;
+	virtual ~Animal() { cout << "Animal 소멸자 호출!" << endl; }
+};
+
+class Dog : public Animal {
+public:
+	Dog(){ cout << "Dog 기본 생성자 호출!" << endl; }
+	void makeSound() {
+		cout << "멍멍" << endl;
+	}
+	virtual ~Dog() { cout << "Dog 소멸자 호출!" << endl; }
+};
+
+class Cat : public Animal {
+public:
+	Cat(){ cout << "Cat 기본 생성자 호출!" << endl; }
+	void makeSound() {
+		cout << "야옹" << endl;
+	}
+	virtual ~Cat() { cout << "Cat 소멸자 호출!" << endl; }
+};
+
+class Cow : public Animal {
+public:
+	Cow(){ cout << "Cow 기본 생성자 호출!" << endl; }
+	void makeSound() {
+		cout << "음모" << endl;
+	}
+	virtual ~Cow() { cout << "Cow 소멸자 호출!" << endl; }
+};
+
+int main() {
+
+	Animal* myAnimal[6];
+
+	for (int i = 0; i < 6; i++) {
+		if (i % 3 == 0) {
+			myAnimal[i] = new Dog();
+		}
+		else if (i % 3 == 1)
+		{
+			myAnimal[i] = new Cat();
+		}
+		else
+			myAnimal [i] = new Cow();
+	}
+
+	for (int i = 0; i < 6; i++)
+	{
+		myAnimal[i]->makeSound();
+	}
+	for (int i = 0; i < 6; i++)
+	{
+		delete myAnimal[i];
+	}
+	return 0;
+}
